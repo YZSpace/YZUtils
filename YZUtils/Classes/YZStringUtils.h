928ede5aa705725df39176272ec5e0689e2f0e2b
huangyazhou <huangyazhou@offcn.com>
2019-06-23 16:50:53 +0800

    0.1.0

---
+++ YZUtils/Classes/YZStringUtils.h
@@ -0,0 +1,22 @@
+//
+//  YZStringUtils.h
+//  YZUtils_Example
+//
+//  Created by 黄亚州 on 2019/6/23.
+//  Copyright © 2019 zone1026. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YZStringUtils : NSObject
+
+///判断字符串中是否包含数字
+/// @param str 字符串
+/// @return 是否包含数字
++ (BOOL)isStringContainNumberWith:(NSString *)str;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YZUtils/Classes/YZStringUtils.m
@@ -0,0 +1,29 @@
+//
+//  YZStringUtils.m
+//  YZUtils_Example
+//
+//  Created by 黄亚州 on 2019/6/23.
+//  Copyright © 2019 zone1026. All rights reserved.
+//
+
+#import "YZStringUtils.h"
+
+@implementation YZStringUtils
+
++ (BOOL)isStringContainNumberWith:(NSString *)str {
+    NSRegularExpression *numberRegular = [NSRegularExpression
+                                          regularExpressionWithPattern:@"[0-9]"
+                                          options:NSRegularExpressionCaseInsensitive
+                                          error:nil];
+    NSInteger count = [numberRegular
+                       numberOfMatchesInString:str
+                       options:NSMatchingReportProgress
+                       range:NSMakeRange(0, str.length)];
+    //count是str中包含[0-9]数字的个数，只要count>0，说明str中包含数字
+    if (count > 0) {
+        return YES;
+    }
+    return NO;
+}
+
+@end
