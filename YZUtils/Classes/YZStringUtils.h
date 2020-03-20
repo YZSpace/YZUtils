cf7598e672b518fe151f7544df452219e0edd36f
huangyazhou <huangyazhou@offcn.com>
2020-03-20 14:23:10 +0800

    测试第三方库

--- YZUtils/Classes/YZStringUtils.h
+++ YZUtils/Classes/YZStringUtils.h
@@ -11,6 +11,10 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface YZStringUtils : NSObject
+///判断字符串中是否包含数字
+/// @param str 字符串
+/// @return 是否包含数字
++ (BOOL)isStringContainNumberWith:(NSString *)str;
 
 @end
 
--- YZUtils/Classes/YZStringUtils.m
+++ YZUtils/Classes/YZStringUtils.m
@@ -10,4 +10,20 @@
 
 @implementation YZStringUtils
 
++ (BOOL)isStringContainNumberWith:(NSString *)str {
+    NSRegularExpression *numberRegular = [NSRegularExpression
+                                          regularExpressionWithPattern:@"[0-9]"
+                                          options:NSRegularExpressionCaseInsensitive
+                                          error:nil];
+    NSInteger count = [numberRegular
+                       numberOfMatchesInString:str
+                       options:NSMatchingReportProgress
+                       range:NSMakeRange(0, str.length)];
+    //count是str中包含[0-9]数字的个数，只要count>0，说明str中包含数字
+    if (count > 0) {
+        return YES;
+    }
+    return NO;
+}
+
 @end
